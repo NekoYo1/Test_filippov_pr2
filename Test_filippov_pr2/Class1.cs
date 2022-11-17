56d48443048dfccfd1cb471c441c7fcdc2d40e5a
NekoYo1 <kostyafear203@gmail.com>
2022-11-17 17:53:35 +1000

    Готово

--- Test_filippov_pr2/Class1.cs
+++ Test_filippov_pr2/Class1.cs
@@ -12,5 +12,12 @@ namespace Test_filippov_pr2
         {
             return a * b + 10;
         }
+    public class Cylinder
+        {
+            public double RectangleArea(double r, double h)
+            {
+                return Math.PI * r * h;
+            }
+        }
     }
 }
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using static Test_filippov_pr2.Geometry;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void RectangleAreaCylinderReturned()
+        {
+            double r = 5;
+            double h = 20;
+            double expected = 1400;
+
+            Cylinder a = new Cylinder();
+            double actual = a.RectangleArea(r, h);
+            Assert.AreEqual(expected, Math.Round(actual));
+        }
+    }
+}
